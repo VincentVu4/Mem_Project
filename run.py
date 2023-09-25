3e57697b759dfe42c185db6cb2ce9468e016ea44
Vincent Vu <vincenhv@uci.edu>
2023-09-24 23:41:23 -0700

    Created Invalid Submissions to reg/login forms

--- run.py
+++ run.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template, url_for, flash
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "3022ca0d1572079ce80786510f474801"
+app.config['SQLALCHEMY_DATABASE_URI'] = "qslite:///site.db"
+
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
 
-@app.route("/register")
+@app.route("/register", methods = ['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    flash("This is a flashed message", 'error')
+    if form.validate_on_submit():
+        flash(f'Account successfully created.')
+        return redirect(url_for('home'))
+
     return render_template("register.html", title = "Register", form=form)
 
-@app.route("/login")
+@app.route("/login", methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'You have successfully logged in.')
+        return redirect(url_for('home'))
     return render_template("login.html", form=form)
 
 if __name__ == "__main__":
