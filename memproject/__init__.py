8ff7dff333bb92a4a995c555011921a6c23c880c
Vincent Vu <vincenhv@uci.edu>
2023-09-25 17:56:15 -0700

    Moved into package and made login successful

---
+++ memproject/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "3022ca0d1572079ce80786510f474801"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+from memproject import routes
--- memproject/forms.py
+++ memproject/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from memproject.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +10,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators = [DataRequired()])
     confirm_password = PasswordField('Confirm_password', validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError("Username has already been taken. Please choose another one.")
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError("Email has already been taken. Please choose another one.")    
 
 
 class LoginForm(FlaskForm):
