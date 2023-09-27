7ed2e5a723fcdf80d31615fe5f2ee054dbcb434e
Vincent Vu <vincenhv@uci.edu>
2023-09-26 18:20:26 -0700

    Finished Account Page

--- memproject/routes.py
+++ memproject/routes.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, url_for, flash, redirect
+import os
+import secrets
+from PIL import Image
+from flask import Flask, render_template, url_for, flash, redirect, request
 from memproject import app, db, bcrypt
-from memproject.forms import RegistrationForm, LoginForm
+from memproject.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from memproject.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
 @app.route("/home")
@@ -20,11 +23,8 @@ def register():
 
         db.session.add(user)
         db.session.commit()
-        flash(f'Account successfully created.')
+        flash(f'Account successfully created.', "success")
         return redirect(url_for('login'))
-
-        
-   
 
     return render_template("register.html", title = "Register", form=form)
 
@@ -37,14 +37,41 @@ def login():
         user = User.query.filter_by(email = form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash(f'Login unsuccessful. Please check your email and password.')
+            flash(f'Login unsuccessful. Please check your email and password.', "danger")
     return render_template("login.html", title = "Login", form=form)
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile-pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods = ['GET', 'POST'])
+@login_required
 def account():
-    return render_template("account.html", title = "Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename = 'profile-pics/' + current_user.image_file)
+    return render_template("account.html", title = "Account", image_file = image_file, form = form)
 
 @app.route("/logout")
 def logout():
