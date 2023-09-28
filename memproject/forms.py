efadbe018063d508f5f37bfc557e817ca286b656
Vincent Vu <vincenhv@uci.edu>
2023-09-27 19:13:17 -0700

    Updated home layout format to a grid layout

--- memproject/forms.py
+++ memproject/forms.py
@@ -53,3 +53,4 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators = [DataRequired()])
     content = TextAreaField('Content', validators = [DataRequired()])
     submit = SubmitField("Post")
+    delete = SubmitField("Delete")
--- memproject/routes.py
+++ memproject/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, abort
 from memproject import app, db, bcrypt
 from memproject.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from memproject.models import User, Post
@@ -105,4 +105,41 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+    return render_template('create_post.html', title='New Post', form=form, legend= "New Post")
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title = post.title, post = post)
+
+@app.route("/post/<int:post_id>/update", methods = ['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        if form.submit.data:
+            post.title = form.title.data
+            post.content = form.content.data
+            db.session.commit()
+            flash("Your post has been updated!", "success")
+            return redirect(url_for("post", post_id = post.id))
+        elif form.delete.data:
+            return redirect(url_for('delete_post', post_id = post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", title = 'Update Post', form = form, legend="Update Post")
+
+@app.route("/post/<int:post_id>/delete", methods = ['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has sucessfully been deleted!", "info")
+    return render_template("home.html")
