36736571c949218e422da0fb38bdb4618fba26f9
Vincent Vu <vincenhv@uci.edu>
2023-09-30 11:27:58 -0700

    Email sending feature (not working (smtp)

--- memproject/forms.py
+++ memproject/forms.py
@@ -55,3 +55,17 @@ class PostForm(FlaskForm):
     picture = FileField('Post Picture', validators = [FileAllowed(['jpg', 'png'])])
     submit = SubmitField("Post")
     delete = SubmitField("Delete")
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user is None:
+            raise ValidationError("There is no account with that email. Please register first.")
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
