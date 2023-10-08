6be3af406236e574660632faec87f8b94cb55950
Vincent Vu <vincenhv@uci.edu>
2023-10-08 13:18:28 -0700

    Changed email

--- memproject/__init__.py
+++ memproject/__init__.py
@@ -17,7 +17,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = os.environ.get("EMAIL_USER")
+app.config['MAIL_USERNAME'] = "iwuvjax@gmail.com"
 app.config['MAIL_PASSWORD'] = "usur hpee hzmy xmec"
 
 
